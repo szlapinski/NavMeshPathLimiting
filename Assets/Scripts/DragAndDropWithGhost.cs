0e699d7379e6aa5d787872463782fb8239624dfe
szlapinski <szlapinski@gmail.com>
2024-07-13 11:44:48 +0200

    text added and also start object transparency fix

--- Assets/Scripts/DragAndDropWithGhost.cs
+++ Assets/Scripts/DragAndDropWithGhost.cs
@@ -1,13 +1,16 @@
 using System;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 namespace software.anthill
 {
     public class DragAndDropWithGhost : MonoBehaviour
     {
-        public PathRenderer pathRenderer;
+        [SerializeField] private PathRenderer pathRenderer;
+        [SerializeField] private TMP_Text maxLengthText;
+        [SerializeField] private TMP_Text pathLengthText;
+        [SerializeField] private TMP_Text dragLengthText;
         [SerializeField] private float maxDistance = 5.0f;
         private bool _isBeingDragged;
         private Plane _dragPlane;
@@ -17,6 +20,7 @@ namespace software.anthill
         private void Start()
         {
             _meshAgent = GetComponent<NavMeshAgent>();
+            UpdateTexts(0.0f, 0.0f);
         }
 
         private void OnMouseDown()
@@ -44,8 +48,9 @@ namespace software.anthill
             if (_meshAgent.CalculatePath(_startGhost.transform.position, calculatedPath))
             {
                 _meshAgent.isStopped = true;
-                var reversedPath = calculatedPath.corners.Reverse().ToArray();
-                if (calculatedPath.Length() > maxDistance)
+                var dragLength = calculatedPath.Length();
+                dragLengthText.text = "drag length: " + dragLength;
+                if (dragLength > maxDistance)
                 {
                     var limitedPath = calculatedPath.LimitFromEnd(maxDistance); 
                     transform.position = new Vector3(
@@ -53,10 +58,12 @@ namespace software.anthill
                             transform.position.y,
                             limitedPath[0].z
                         );
+                    UpdateTexts(maxDistance, dragLength);
                     pathRenderer.UpdatePath(limitedPath);
                 }
                 else
                 {
+                    UpdateTexts(dragLength, dragLength);
                     pathRenderer.UpdatePath(calculatedPath.corners);
                 }
             }
@@ -72,6 +79,7 @@ namespace software.anthill
             pathRenderer.UpdatePath(Array.Empty<Vector3>());
             ChangeTransparency(gameObject, 1.0f);
             Destroy(_startGhost);
+            UpdateTexts(0.0f, 0.0f);
         }
 
         private GameObject CreateGhost()
@@ -86,10 +94,19 @@ namespace software.anthill
         private GameObject ChangeTransparency(GameObject ghost, float alpha)
         {
             var meshRenderer = ghost.GetComponentInChildren<MeshRenderer>();
-            var materialColor = meshRenderer.material.color;
+            var material = new Material(meshRenderer.material);
+            var materialColor = material.color;
             materialColor.a = alpha;
-            meshRenderer.material.color = materialColor;
+            material.color = materialColor;
+            meshRenderer.material = material;
             return ghost;
+        }
+
+        private void UpdateTexts(float pathLength, float dragLength)
+        {
+            maxLengthText.text = "max length: " + maxDistance.ToString("F1");
+            pathLengthText.text = "path length: " + pathLength.ToString("F1");;
+            dragLengthText.text = "drag length: " + dragLength.ToString("F1");;
         }
     }
 }
